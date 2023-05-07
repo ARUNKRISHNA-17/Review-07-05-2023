96f826d0002bd4fbdbf511168c16fdf60df4c3bc
Arunkrishna E S <arunkrishnaips2003@gmail.com>
2023-05-07 13:59:54 +0530

    Initial commit

---
+++ Review-07-0-2023/src/main/java/com/review/demo/Review0702023Application.java
@@ -0,0 +1,13 @@
+package com.review.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Review0702023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Review0702023Application.class, args);
+	}
+
+}
---
+++ Review-07-0-2023/src/test/java/com/review/demo/Review0702023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.review.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Review0702023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
